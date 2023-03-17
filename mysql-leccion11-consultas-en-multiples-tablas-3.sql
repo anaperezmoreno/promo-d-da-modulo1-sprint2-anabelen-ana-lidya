53c38e963400de641507d9e820484fde9113a281
Lidya <lidyadescalssamper@gmail.com>
2023-03-17 11:41:58 +0100

    python a medias, archivo sql vacio

---
+++ mysql-leccion11-consultas-en-multiples-tablas-3.sql
@@ -0,0 +1,40 @@
+
+/* 1. Extraer toda la información sobre las compañias que tengamos en la BBDD
+Nuestros jefes nos han pedido que creemos una query que nos devuelva todos los clientes y proveedores que tenemos en la BBDD. Mostrad la ciudad a la que pertenecen, el nombre de la empresa y el nombre del contacto, además de la relación (Proveedor o Cliente). Pero importante! No debe haber duplicados en nuestra respuesta. La columna Relationship no existe y debe ser creada como columna temporal. Para ello añade el valor que le quieras dar al campo y utilizada como alias Relationship.
+Nota: Deberás crear esta columna temporal en cada instrucción SELECT.*/
+
+SELECT city, company_name, contact_name, "Cliente" AS "Relationship" 
+	FROM customers
+	UNION 
+		SELECT city, company_name, contact_name, "Proveedor" AS "Relationship"
+		FROM suppliers;
+
+/* 2. Extraer todos los pedidos gestinados por "Nancy Davolio"
+En este caso, nuestro jefe quiere saber cuantos pedidos ha gestionado "Nancy Davolio", una de nuestras empleadas.
+Nos pide todos los detalles tramitados por ella.*/
+
+SELECT *
+	FROM orders
+	WHERE employee_id IN 
+					(SELECT employee_id FROM employees WHERE first_name = "Nancy" AND last_name = "Davolio");
+
+/* 3. Extraed todas las empresas que no han comprado en el año 1997
+En este caso, nuestro jefe quiere saber qué empresas no han comprado en el año 1997.
+Pista . Para extraer el año de una fecha, podemos usar el estamento year. Más documentación sobre este método.*/
+
+SELECT company_name
+	FROM customers
+	WHERE customer_id  NOT IN 
+						(SELECT customer_id FROM orders WHERE YEAR(order_date) = 1997);
+
+/* 4. Extraed toda la información de los pedidos donde tengamos "Konbu"
+Estamos muy interesados en saber como ha sido la evolución de la venta de Konbu a lo largo del tiempo. 
+Nuestro jefe nos pide que nos muestre todos los pedidos que contengan "Konbu".
+Pista 💡 En esta query tendremos que combinar conocimientos adquiridos en las lecciones anteriores como por ejemplo el 
+INNER JOIN.*/
+
+SELECT * 
+	FROM orders INNER JOIN order_details 
+	ON orders.order_id = order_details.order_id
+	WHERE product_id IN 
+					(SELECT product_id FROM products WHERE product_name = "Konbu");
--- mysql-leccion8-consultas-avanzadas-2.sql
+++ mysql-leccion8-consultas-avanzadas-2.sql
@@ -1,72 +1,64 @@
--- Relación entre número de pedidos y máxima carga:
--- Desde logística nos piden el número de pedidos y la máxima cantidad de carga de entre los mismos (freight) que 
--- han sido enviados por cada empleado (mostrando el ID de empleado en cada caso).
+
+
+/* Relación entre número de pedidos y máxima carga:
+Desde logística nos piden el número de pedidos y la máxima cantidad de carga de entre los mismos (freight) que 
+han sido enviados por cada empleado (mostrando el ID de empleado en cada caso).*/
 
 SELECT COUNT(order_id), MAX(freight), employee_id 
-FROM orders 
-GROUP BY employee_id;
+	FROM orders 
+	GROUP BY employee_id;
 
-
--- Descartar pedidos sin fecha y ordénalos:
--- Una vez han revisado los datos de la consulta anterior, nos han pedido afinar un poco más el "disparo".
- -- En el resultado anterior se han incluido muchos pedidos cuya fecha de envío estaba vacía, 
- -- por lo que tenemos que mejorar la consulta en este aspecto. También nos piden que ordenemos los resultados 
- -- según el ID de empleado para que la visualización sea más sencilla.
+/* Descartar pedidos sin fecha y ordénalos:
+Una vez han revisado los datos de la consulta anterior, nos han pedido afinar un poco más el "disparo".
+En el resultado anterior se han incluido muchos pedidos cuya fecha de envío estaba vacía, 
+por lo que tenemos que mejorar la consulta en este aspecto. También nos piden que ordenemos los resultados 
+según el ID de empleado para que la visualización sea más sencilla.*/
  
 SELECT COUNT(order_id) AS numero_pedidos,  MAX(freight) AS carga_maxima, employee_id
-FROM orders 
-WHERE shipped_date IS NOT NULL 
-GROUP BY employee_id
-ORDER BY employee_id; 
+	FROM orders 
+	WHERE shipped_date IS NOT NULL 
+	GROUP BY employee_id
+	ORDER BY employee_id; 
  
--- Números de pedidos por día: AGRUPAR POR FECHA PARA VERLO EN UNA SOLA FILA - hay que agrupar por fecha para contarlas!
--- El siguiente paso en el análisis de los pedidos va a consistir en conocer mejor la distribución de los mismos 
--- según las fechas. Por lo tanto, tendremos que generar una consulta que nos saque el número de pedidos para cada día, 
--- mostrando de manera separada el día (DAY()), el mes (MONTH()) y el año (YEAR()).
+/* Números de pedidos por día: AGRUPAR POR FECHA PARA VERLO EN UNA SOLA FILA - hay que agrupar por fecha para contarlas!
+El siguiente paso en el análisis de los pedidos va a consistir en conocer mejor la distribución de los mismos 
+según las fechas. Por lo tanto, tendremos que generar una consulta que nos saque el número de pedidos para cada día, 
+mostrando de manera separada el día (DAY()), el mes (MONTH()) y el año (YEAR()).*/
 
-SELECT COUNT(*), order_date,
-DAY(order_date), MONTH(order_date), YEAR(order_date)
-FROM orders 
-GROUP BY order_date;
+SELECT COUNT(*), order_date, DAY(order_date), MONTH(order_date), YEAR(order_date)
+	FROM orders 
+	GROUP BY order_date;
 
+/* Número de pedidos por mes y año:
+La consulta anterior nos muestra el número de pedidos para cada día concreto, pero esto es demasiado detalle. 
+Genera una modificación de la consulta anterior para que agrupe los pedidos por cada mes concreto de cada año*/
 
+SELECT COUNT(*), order_date, MONTH(order_date), YEAR(order_date)
+	FROM orders 
+	GROUP BY order_date;
 
+/* Seleccionad las ciudades con 4 o más empleadas:
+Desde recursos humanos nos piden seleccionar los nombres de las ciudades con 4 o más empleadas 
+de cara a estudiar la apertura de nuevas oficinas.*/
 
+SELECT city, COUNT(employee_id)
+	FROM employees
+	GROUP BY city
+	HAVING COUNT(employee_id) >= 4;
 
--- Número de pedidos por mes y año:
--- La consulta anterior nos muestra el número de pedidos para cada día concreto, pero esto es demasiado detalle. 
--- Genera una modificación de la consulta anterior para que agrupe los pedidos por cada mes concreto de cada año
-
-SELECT COUNT(*), order_date,
-MONTH(order_date), YEAR(order_date)
-FROM orders 
-GROUP BY order_date;
-
-
--- Seleccionad las ciudades con 4 o más empleadas:
--- Desde recursos humanos nos piden seleccionar los nombres de las ciudades con 4 o más empleadas 
--- de cara a estudiar la apertura de nuevas oficinas.
-
-SELECT 
-    city, COUNT(employee_id)
-FROM
-    employees
-GROUP BY city
-HAVING COUNT(employee_id) >= 4;
-
--- Cread una nueva columna basándonos en la cantidad monetaria:
--- Necesitamos una consulta que clasifique los pedidos en dos categorías ("Alto" y "Bajo") 
--- en función de la cantidad monetaria total que han supuesto: por encima o por debajo de 2000 euros.*/
+ /* Cread una nueva columna basándonos en la cantidad monetaria:
+Necesitamos una consulta que clasifique los pedidos en dos categorías ("Alto" y "Bajo") 
+en función de la cantidad monetaria total que han supuesto: por encima o por debajo de 2000 euros.*/
 
 
 
 SELECT order_id, SUM(quantity*unit_price) AS 'total_pedido',
-		CASE 
-			WHEN SUM(quantity*unit_price) > 2000 THEN "Alto"
-			ELSE 'Bajo'
-            END AS 'Etiqueta'
-		FROM order_details
-		GROUP BY order_id;
+	CASE 
+		WHEN SUM(quantity*unit_price) > 2000 THEN "Alto"
+		ELSE 'Bajo'
+		END AS 'Etiqueta'
+	FROM order_details
+	GROUP BY order_id;
 
 
 
